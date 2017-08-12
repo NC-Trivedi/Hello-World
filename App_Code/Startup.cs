0fea6217a2e9c6690d6264446af42d94708a827c
unknown <nitantct@gmail.com>
2017-08-12 21:15:50 +0530

    Version: 2017.08.01 Description: Moved project to Git

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NCT_Web.Startup))]
+namespace NCT_Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
